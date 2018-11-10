83a3883ef94e63bfd6ba97de04d7b04e4b2ffee6
EtienneBerube <tiennebrb@gmail.com>
2018-11-10 16:45:53 -0500

    Created Project

---
+++ DontTrashMyHouse/src/main/java/com/bostonHacks/DontTrashMyHouse/DontTrashMyHouseApplication.java
@@ -0,0 +1,12 @@
+package com.bostonHacks.DontTrashMyHouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DontTrashMyHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DontTrashMyHouseApplication.class, args);
+	}
+}
