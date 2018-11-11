f9e2a994fd3126d8d163fd6c03bc6914b4cdf979
EtienneBerube <tiennebrb@gmail.com>
2018-11-10 22:18:52 -0500

    Models done

---
+++ DontTrashMyHouse/DontTrashMyHouse/src/main/java/com/bostonHacks/DontTrashMyHouse/models/House.java
@@ -0,0 +1,106 @@
+package com.bostonHacks.DontTrashMyHouse.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="houses")
+public class House {
+    @Id
+    private String id;
+    private String address;
+    private String latitude;
+    private String longitude;
+    private int appNumber;
+    private User owner;
+    private boolean used;
+    private int rating;
+    private int code;
+
+    public House() {
+    }
+
+    public House(String id, String address, String latitude, String longitude, int appNumber, User owner, boolean used, int rating, int code) {
+        this.id = id;
+        this.address = address;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.appNumber = appNumber;
+        this.owner = owner;
+        this.used = used;
+        this.rating = rating;
+        this.code = code;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public int getAppNumber() {
+        return appNumber;
+    }
+
+    public void setAppNumber(int appNumber) {
+        this.appNumber = appNumber;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public boolean isUsed() {
+        return used;
+    }
+
+    public void setUsed(boolean used) {
+        this.used = used;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+}
---
+++ DontTrashMyHouse/DontTrashMyHouse/src/main/java/com/bostonHacks/DontTrashMyHouse/models/Rental.java
@@ -0,0 +1,65 @@
+package com.bostonHacks.DontTrashMyHouse.models;
+
+import org.springframework.data.annotation.Id;
+
+import java.util.Date;
+
+public class Rental {
+    @Id
+    private String id;
+    private House house;
+    private User renter;
+    private Date startDate;
+    private Date endDate;
+
+    public Rental() {
+    }
+
+    public Rental(String id, House house, User renter, Date startDate, Date endDate) {
+        this.id = id;
+        this.house = house;
+        this.renter = renter;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public House getHouse() {
+        return house;
+    }
+
+    public void setHouse(House house) {
+        this.house = house;
+    }
+
+    public User getRenter() {
+        return renter;
+    }
+
+    public void setRenter(User renter) {
+        this.renter = renter;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+}
